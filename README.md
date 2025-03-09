ce3a728ce47f6a73df5c0951fe6e67679b11d119
aurora0x27 <qingfengzl0831@163.com>
2025-03-09 20:54:02 +0800

    init the repo

---
+++ README.md
@@ -0,0 +1,5 @@
+# Editor from scratch
+
+Learn rust from scratch by writing a text editor from scratch.
+
+Tutorial is [here](https://flenker.blog/hecto-chapter-1/)
---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use std::io::{self, Read};
+
+fn main() {
+    for b in io::stdin().bytes() {
+        let c = b.unwrap() as char;
+        if c == 'q' {
+            break;
+        }
+        println!("Receiving char `{}`", c);
+    }
+}
